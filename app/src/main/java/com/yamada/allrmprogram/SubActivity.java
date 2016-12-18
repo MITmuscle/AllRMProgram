4c61431a9d1eb8f757d713e0b265ac00d5a02581
t-YAMADA <16043065@mmm.muroran-it.ac.jp>
2016-12-18 23:30:41 +0900

    add back button

--- app/src/main/java/com/yamada/allrmprogram/SubActivity.java
+++ app/src/main/java/com/yamada/allrmprogram/SubActivity.java
@@ -2,6 +2,8 @@ package com.yamada.allrmprogram;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by yamada-PC on 2016/12/18.
@@ -14,5 +16,12 @@ public class SubActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
 
+        Button button = (Button)findViewById(R.id.back_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
