f8a5d7ec5244b6df4da56e5e809d01ee3796ae9d
t-YAMADA <16043065@mmm.muroran-it.ac.jp>
2016-12-18 17:40:04 +0900

    add calc & display of RM

--- app/src/main/java/com/yamada/allrmprogram/MainActivity.java
+++ app/src/main/java/com/yamada/allrmprogram/MainActivity.java
@@ -3,6 +3,7 @@ package com.yamada.allrmprogram;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.StringBuilderPrinter;
 import android.view.*;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -34,17 +35,66 @@ public class MainActivity extends AppCompatActivity {
 
                 if(s_weight.length()!=0){
                     int i_weight = Integer.valueOf(s_weight);
-                    //1RMの計算(挙上重量 / 33.3 * 挙上回数 + 挙上重量)
-                    double ex_rm = i_weight / 33.3 * reps + i_weight;
-                    //int型（整数値）に変換
-                    int ex_i_rm = (int)ex_rm;
-                    //表示のために文字列に変換
-                    String s_rm = String.valueOf(ex_i_rm);
-                    //確認
-                    Log.d("MAIN","計算した1RM = "+s_rm);
+                    //1RMの計算(挙上重量 / 40 * 挙上回数 + 挙上重量)
+                    //double rm1 = i_weight / 40 * reps + i_weight;
+                    //MAX重量）＝（使用重量）×｛１＋（Reps／４０）｝
+                    double rm1 = 0;
+                    rm1 = i_weight * (reps + 40) / 40;
+                    toast(String.valueOf(reps));
 
-                    TextView text = (TextView)findViewById(R.id.rm1);
-                    text.setText(s_rm);
+                    //int型（整数値）に変換
+                    int i_rm1 = (int)rm1;
+                    //表示のために文字列に変換
+                    String s_rm1 = String.valueOf(i_rm1);
+                    Log.d("MAIN",s_rm1);
+
+                    double[] x = new double[21];
+                    String[] s_x = new String[21];
+                    int[] i_x = new int[21];
+
+                    //それぞれのRMを計算
+                    //100kg=Xkg÷40×3回+Xkg
+                    for(int i=0;i<=20;i++) {
+                        x[i] = (33.3 * rm1) / (i + 33.3);
+                        i_x[i] = (int)x[i];
+                        s_x[i] = String.valueOf(i_x[i]);
+                    }
+
+                    //それぞれのRMを表示
+                    TextView text1 = (TextView)findViewById(R.id.rm1_text);
+                    TextView text2 = (TextView)findViewById(R.id.rm2_text);
+                    TextView text3 = (TextView)findViewById(R.id.rm3_text);
+                    TextView text5 = (TextView)findViewById(R.id.rm5_text);
+                    TextView text7 = (TextView)findViewById(R.id.rm7_text);
+                    TextView text8 = (TextView)findViewById(R.id.rm8_text);
+                    TextView text10 = (TextView)findViewById(R.id.rm10_text);
+                    TextView text12 = (TextView)findViewById(R.id.rm12_text);
+                    TextView text15 = (TextView)findViewById(R.id.rm15_text);
+                    TextView text20 = (TextView)findViewById(R.id.rm20_text);
+
+                    text1.setText(s_x[1-1]);
+                    text2.setText(s_x[2-1]);
+                    text3.setText(s_x[3-1]);
+                    text5.setText(s_x[5-1]);
+                    text7.setText(s_x[7-1]);
+                    text8.setText(s_x[8-1]);
+                    text10.setText(s_x[10-1]);
+                    text12.setText(s_x[12-1]);
+                    text15.setText(s_x[15-1]);
+                    text20.setText(s_x[20-1]);
+
+                   /* switch(s_reps){
+                        case "1":text1.setText(s_weight);break;
+                        case "2":text2.setText(s_weight);break;
+                        case "3":text3.setText(s_weight);break;
+                        case "5":text5.setText(s_weight);break;
+                        case "7":text7.setText(s_weight);break;
+                        case "8":text8.setText(s_weight);break;
+                        case "10":text10.setText(s_weight);break;
+                        case "12":text12.setText(s_weight);break;
+                        case "15":text15.setText(s_weight);break;
+                        case "20":text20.setText(s_weight);break;
+                    }*/
                 }else{
                     //重量未入力時の処理
                     toast("挙上重量を入力してください");
@@ -71,8 +121,10 @@ public class MainActivity extends AppCompatActivity {
         Log.d("MAIN","RM1");
         EditText edit = (EditText)findViewById(R.id.editText);
         if(edit.length()!=0) {
+            TextView rm_1 = (TextView)findViewById(R.id.rm1_text);
+            String s_rm_1 = rm_1.getText().toString();
 
-            Log.d("MAIN","if");
+            Log.d("MAIN",s_rm_1);
         }
         else{
             Log.d("MAIN","else");
