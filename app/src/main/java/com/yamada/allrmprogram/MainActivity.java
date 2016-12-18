dc0471d4164923a23f30f8804f2b9b42e07a2d4e
t-YAMADA <16043065@mmm.muroran-it.ac.jp>
2016-12-18 13:24:12 +0900

    add main screen

--- app/src/main/java/com/yamada/allrmprogram/MainActivity.java
+++ app/src/main/java/com/yamada/allrmprogram/MainActivity.java
@@ -2,6 +2,10 @@ package com.yamada.allrmprogram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.*;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //TextView押下時の挙動チェック
+    public void RM1_OnClick(View view){
+        Log.d("MAIN","RM1");
+    }
+
+    public void RM2_OnClick(View view){
+        Log.d("MAIN","RM2");
+    }
+
+    public void RM3_OnClick(View view){
+        Log.d("MAIN","RM3");
+    }
+
+    public void RM5_OnClick(View view){
+        Log.d("MAIN","RM5");
+    }
+
+    public void RM7_OnClick(View view){
+        Log.d("MAIN","RM7");
+    }
+
+    public void RM8_OnClick(View view){
+        Log.d("MAIN","RM8");
+    }
+
+    public void RM10_OnClick(View view){
+        Log.d("MAIN","RM10");
+    }
+
+    public void RM12_OnClick(View view){
+        Log.d("MAIN","RM12");
+    }
+
+    public void RM15_OnClick(View view){
+        Log.d("MAIN","RM15");
+    }
+
+    public void RM20_OnClick(View view){
+        Log.d("MAIN","RM20");
+    }
+
 }
